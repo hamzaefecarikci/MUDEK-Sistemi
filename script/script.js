9fdf60403157fda0f67c7ed2e6a9e10a5ad6bc39
Hamza Efe Çarıkcı <f_efa@hotmail.com>
2024-05-09 17:36:11 +0300

    Ders ekleme sayfası yapıldı

---
+++ script/script.js
@@ -0,0 +1,47 @@
+let ogrenmeCiktisi = [
+    
+];
+
+displayTask();
+
+
+function displayTask()
+{
+    let ol = document.getElementById("task-list");
+    ol.innerHTML = "";
+
+    for(let ogrenme of ogrenmeCiktisi)
+        {
+            let li = `<li class="list-group-item">${ogrenme.gorevAdi}</li>
+            `;
+
+            ol.insertAdjacentHTML("beforeend", li);
+        }
+}
+
+    document.querySelector("#btnAddNewTask").addEventListener("click", newTask);
+    document.querySelector("#btnAddNewTask").addEventListener("keypress", function(event){
+        if(event.key == "Enter")
+        {
+            document.getElementById("btnAddNewTask").click();
+        }
+    });
+
+function newTask(event)
+{
+    let taskInput = document.querySelector("#textTaskName");
+
+    if(taskInput.value == "")
+    {
+        alert("Öğrenme Çıktısı giriniz");
+    }
+
+    else
+    {
+        ogrenmeCiktisi.push({"id": ogrenmeCiktisi.length+1, "gorevAdi": taskInput.value});
+        taskInput.value = "";
+        displayTask();
+    }
+
+    event.preventDefault();
+}
