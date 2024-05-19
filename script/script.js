28a32316ebfa031b345812424fef83fd8b69fd05
Hamza Efe Çarıkcı <f_efa@hotmail.com>
2024-05-19 22:34:57 +0300

    Ders Ekle Butonu Duzeltildi

--- script/script.js
+++ script/script.js
@@ -1,10 +1,35 @@
+const btnDersEkle = document.querySelector("#btnDersEkle").addEventListener("click",DERSEKLE);
+
+function DERSEKLE()
+{
+    let dersKontrol = document.querySelector(".form-floating .dersSecim").value;
+
+    if(dersKontrol == "select")
+    {
+        alert("Lütfen Ders Seçiniz!");
+    }
+
+    else
+    {
+        document.querySelector("#seciliDers").value = dersKontrol;
+    }
+}
+
+
 const btnDevam = document.querySelector(".btnDevam").addEventListener("click",DEVAM);
 
 function DEVAM(event)
 {
-    document.querySelector(".lesson-box").classList.remove("active");
-    document.querySelector(".tablo-box").classList.add("active");
+    document.querySelector(".lesson-box").classList.add("hidden");
+    document.querySelector(".degerlendirme").classList.remove("hidden");
     window.scrollTo(0, 0);
     
     event.preventDefault();
+}
+
+const btnDegerlendirme = document.querySelector(".degelendirme .btn").addEventListener("click",GOSTER);
+
+function GOSTER()
+{
+    
 }
